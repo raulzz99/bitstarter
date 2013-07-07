3f6400f281f1e2f55b1643a8f60e2f2d27d2efe8
raulzz99 <raulzz99@gmail.com>
2013-07-07 03:46:10 +0000

    1234

--- web.js
+++ web.js
@@ -1,10 +1,20 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
+var text = '';
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+
+text = fs.readFileSync('index.html','utf 8');
+var buffer = new Buffer();
+var bftxt = buffer.toString('utf-8 ',0,text.length);
+resonse.send(bftxt);
+//console.log(bftxt);
+
+
+console.log(text);
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
